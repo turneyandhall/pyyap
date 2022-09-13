09302cebdf8b7fbbe7898916b91327d77c98d0ae
turneyandhall <richard@turneyandhall.com>
2022-09-13 13:21:17 +0100

    success message

---
+++ app/routes/success-message.tsx
@@ -0,0 +1,23 @@
+import type { MetaFunction } from "@remix-run/node";
+import TextContainer from "~/components/textContainer";
+import { Box, Heading, } from '@chakra-ui/react'
+
+
+export const meta: MetaFunction = () => {
+  return {
+    description:
+      "Pyyap form success message",
+  }
+}
+
+
+export default function SuccessMessage() {
+  return (
+    <Box bg="pyyap.500" minH="100vh">
+        <TextContainer>
+            <Heading as='h2' color="white" fontFamily="DM Serif Display, serif" lineHeight="1.4" fontWeight="400">Thank You!</Heading>
+        </TextContainer>
+    </Box>
+
+  )
+}
